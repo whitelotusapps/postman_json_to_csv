abd97c8e69866704d843c1ebfe1d6487aa34f5a3
Zack Olinger <github@therealzackolinger.com>
2023-06-22 18:24:57 -0500

    Updating .gitignore

--- app.py
+++ app.py
@@ -1,68 +1,90 @@
+# Import the necessary libraries
 import streamlit as st
-import csv
 import json
+import csv
 import time
 
+# Use the Streamlit cache to speed up repeated loading of the same file
+@st.cache_data
+def load_data_from_file(uploadfile):
+    # Load the JSON data from the file
+    data = json.load(uploadfile)
+    return data
 
-def export_to_csv(data, keys):
-    timestr = time.strftime("%Y%m%d-%H%M%S")
-    with open(f"{timestr} - output.csv", "w", newline="") as file:
+# Use the Streamlit cache to speed up repeated parsing of the same text
+@st.cache_data
+def load_data_from_text(json_text):
+    # Load the JSON data from the text
+    data = json.loads(json_text)
+    return data
+
+# Function to export the selected data to a CSV file
+def export_to_csv(data, keys, output_file):
+    with open(output_file, "w", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=keys)
         writer.writeheader()
         writer.writerows(data)
 
-
 def main():
     st.title("JSON to CSV Converter")
-    json_input = st.text_area("Paste the JSON here", height=400)
-    try:
-        data = json.loads(json_input)
-    except ValueError:
-        st.error("Invalid JSON")
-        return
 
-    keys = set()
-    for item in data:
-        keys.update(item[list(item.keys())[0]].keys())
+    # Let the user choose whether to input a file or text
+    input_option = st.radio("Select input type", ("File", "Text"))
 
-    selected_keys = st.multiselect(
-        "Select the keys to export", list(keys), default=list(keys)
-    )
+    data = None
 
-    if not selected_keys:
-        st.warning("No keys selected")
-        return
+    # Handle the file input option
+    if input_option == "File":
+        uploaded_file = st.file_uploader("Choose a JSON file", type='json')
+        if uploaded_file is not None:
+            data = load_data_from_file(uploaded_file)
 
-    keys_ordered = (
-        st.text_area(
-            "Arrange the selected keys (one key per line)", "\n".join(selected_keys)
+    # Handle the text input option
+    elif input_option == "Text":
+        json_text = st.text_area("Paste JSON data")
+        if json_text:
+            data = load_data_from_text(json_text)
+
+    if data:
+        # Get the keys from the first item in the data
+        keys = set(next(iter(data[0].values())).keys())
+
+        # Let the user arrange the keys
+        keys_ordered = (
+            st.text_area(
+                "Arrange the keys (one key per line)", "\n".join(keys), height=len(keys)*25
+            )
+            .strip()
+            .split("\n")
         )
-        .strip()
-        .split("\n")
-    )
 
-    selected_keys_ordered = [
-        key.strip() for key in keys_ordered if key.strip() in selected_keys
-    ]
+        # Only keep the keys that the user entered
+        selected_keys_ordered = [
+            key.strip() for key in keys_ordered if key.strip() in keys
+        ]
 
-    if len(selected_keys_ordered) != len(selected_keys):
-        st.warning("Some selected keys were not found in the ordered list")
+        if len(selected_keys_ordered) != len(keys):
+            st.warning("Some keys were not found in the ordered list")
 
-    if st.button("Export to CSV"):
-        selected_data = []
-        for item in data:
-            selected_item = {
-                k: v
-                for k, v in item[list(item.keys())[0]].items()
-                if k in selected_keys_ordered
-            }
-            selected_data.append(selected_item)
-        if selected_data:
-            export_to_csv(selected_data, selected_keys_ordered)
-            st.success("CSV file exported successfully")
-        else:
-            st.warning("No data selected for export")
+        # Create a timestamp for the filename
+        timestampStr = time.strftime("%Y-%m-%d_%H-%M-%S")
+        output_file = f"JSONtoCSV_{timestampStr}.csv"
 
+        # Handle the export to CSV button
+        if st.button("Export to CSV"):
+            selected_data = []
+            for item in data:
+                selected_item = {
+                    k: v
+                    for k, v in next(iter(item.values())).items()
+                    if k in selected_keys_ordered
+                }
+                selected_data.append(selected_item)
+            if selected_data:
+                export_to_csv(selected_data, selected_keys_ordered, output_file)
+                st.success(f"CSV file exported successfully to {output_file}")
+            else:
+                st.warning("No data selected for export")
 
 if __name__ == "__main__":
     main()
